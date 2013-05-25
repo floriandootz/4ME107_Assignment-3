e016e8364044611ef12e519b91f19796e08b50e1
pl3xus <plexus@chilloutsofa.com>
2013-05-25 19:26:28 +0200

    - Working PHP Service. - Working NodeJS Service. - Both NodeJS Services Managed by one File.

--- nodejsservices/hotels.js
+++ nodejsservices/hotels.js
@@ -1,74 +1,64 @@
-// Imports
-var http = require('http');
-var url = require('url');
-var parseString = require('xml2js').parseString;
-var querystring = require("querystring");
-
-// Server Vars
-var ip = '127.0.0.1';
-var port = 1337;
-
-var srv = http.createServer(function (req, res)
+(function()
 {
-	res.writeHead(200, {'Content-Type': 'application/json'});
-	//res.writeHead(200, {'Connection': 'keep-alive'});
-	console.log("----start----");
-	var returnArray = [];
-	
-	// URL Params
-	var url_parts = url.parse(req.url, true);
-	var query = url_parts.query;
-	var callback = '';
-	var priceMin = 0;
-	var priceMax = 1000;
-	
-	// GET Params
-	if (query.callback) {
-		callback = query.callback;
-		console.log('Callback Function: ' +query.callback);
-	}
-	if (query.priceMin) {
-		priceMin = query.priceMin;
-	}
-	if (query.priceMax) {
-		priceMax = query.priceMax;
-	}
-	
-	// Functionality
-	var hotelUrl = 'http://www.kayak.com/h/rss/hotelrss/IS/Reykjavik?mc=EUR';
-	http.get(hotelUrl, function(hotelRes) {
-		var xml = "";
-		hotelRes.on('data', function(chunk) {
-			xml += chunk;
-		});
-		hotelRes.on('end', function() {
-			parseString(xml, function (err, result) {
-				var array = result.rss.channel[0].item;
-				array.forEach( function(item) {
-					var price = parseInt(item['kyk:price'][0]);
-					if (price <= priceMax && price >= priceMin) {
-						var obj = new Object();
-						obj.name = item.title[0];
-						obj.externalLink = item.link[0];
-						obj.price = price;
-						obj.stars = item['kyk:stars'][0];
-						obj.image = item['kyk:thumbnail'][0];
-						returnArray.push(obj);
+	// Imports
+	var http = require('http');
+	var parseString = require('xml2js').parseString;
+
+	module.exports.run = function(req, res, query)
+	{
+		console.log("----start----");
+		var returnArray = [];
+		
+		// URL Params
+		var callback = '';
+		var priceMin = 0;
+		var priceMax = 1000;
+		
+		// GET Params
+		if (query.callback) {
+			callback = query.callback;
+			console.log('Callback Function: ' +query.callback);
+		}
+		if (query.priceMin) {
+			priceMin = query.priceMin;
+		}
+		if (query.priceMax) {
+			priceMax = query.priceMax;
+		}
+		
+		// Functionality
+		var hotelUrl = 'http://www.kayak.com/h/rss/hotelrss/IS/Reykjavik?mc=EUR';
+		http.get(hotelUrl, function(hotelRes) {
+			var xml = "";
+			hotelRes.on('data', function(chunk) {
+				xml += chunk;
+			});
+			hotelRes.on('end', function() {
+				parseString(xml, function (err, result) {
+					var array = result.rss.channel[0].item;
+					array.forEach( function(item) {
+						var price = parseInt(item['kyk:price'][0]);
+						if (price <= priceMax && price >= priceMin) {
+							var obj = new Object();
+							obj.name = item.title[0];
+							obj.externalLink = item.link[0];
+							obj.price = price;
+							obj.stars = item['kyk:stars'][0];
+							obj.image = item['kyk:thumbnail'][0];
+							returnArray.push(obj);
+						}
+					});
+					console.log("-----end-----");
+					// Output
+					if (callback) {
+						res.write(callback + '(' + JSON.stringify(returnArray) + ');');
 					}
+					else {
+						res.write(JSON.stringify(returnArray));
+					}
+					res.end();
 				});
-				console.log("-----end-----");
-				// Output
-				if (callback) {
-					res.write(callback + '(' + JSON.stringify(returnArray) + ');');
-				}
-				else {
-					res.write(JSON.stringify(returnArray));
-				}
-				res.end();
 			});
 		});
-	});
-});
-
-srv.listen(port, ip);
-console.log('Server running at http://'+ip+':'+port+'/');
+	}
+}());
--- nodejsservices/services.js
+++ nodejsservices/services.js
@@ -1,8 +1,9 @@
 // Imports
 var http = require('http');
 var url = require('url');
-var parseString = require('xml2js').parseString;
 var querystring = require("querystring");
+var weather = require('./weather.js');
+var hotels = require('./hotels.js');
 
 // Server Vars
 var ip = '127.0.0.1';
@@ -10,7 +11,30 @@ var port = 1337;
 
 var srv = http.createServer(function (req, res)
 {
-	// res.writeHead(200, {'Content-Type': 'application/json'});
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+	
+	if (query.service) {
+		service = query.service;
+		res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+	
+		if (service == 'hotels') {
+			hotels.run(req, res, query);
+		}
+		else if (service == 'weather') {
+			weather.run(req, res, query);
+		}
+		else {
+			console.log("nothing to do");
+			console.log("-----end-----");
+			res.end();
+		}
+	}
+	else {
+		console.log("nothing to do");
+		console.log("-----end-----");
+		res.end();
+	}
 	
 });
 
